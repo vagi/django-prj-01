e1097ebe16c0cc4f8f3352079c907e79368b23b5
Vagif Molla Air <vagif69@gmail.com>
2021-08-04 16:45:45 +0300

    Add new model Question, make migration

--- poll_site/polls/models.py
+++ poll_site/polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    id = models.AutoField(primary_key=True)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date_published')
+    opened = models.BooleanField()
+
