60fa88137cb60f9a858e71d7b400ff78278fd456
Vagif Molla Air <vagif69@gmail.com>
2021-08-08 17:54:37 +0300

    Add new views: detail, results, vote; changed index view

--- poll_site/polls/views.py
+++ poll_site/polls/views.py
@@ -2,7 +2,23 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse("output")
+
+
+def detail(request, question_id):
+    return HttpResponse("You are looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You are looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
